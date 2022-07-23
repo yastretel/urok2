383321bbc5e1c551e3a50fdcb30c2d2c3529e153
yastretel <yas.tretel@gmail.com>
2022-07-23 05:26:27 +0300

    Бот сделан...

--- task3.py
+++ task3.py
@@ -1,7 +1,12 @@
+import telebot
+
+
+
+
 HELP = """
 help - напечатать строку по программе.
 add - добавить задачу в рассписание(название задачи запрашиваем у пользователя).
-show - показать рассписание
+show - показать рассписание на определённое число
 """
 today = []
 tomorrow = []
@@ -9,30 +14,36 @@ other = []
 
 run = True
 
+def show():
+    global data1
+    data1 = input("Введите дату на которую хотите увидеть рассписание:")
+    if data1 == "Сегодня":
+        print('-', today)
+    elif data1 == "Завтра":
+        print('-', tomorrow)
+    else:
+        print('-', other)
+
+def add():
+    task = input("Введите название задачи:")
+    data = input("Введите дату задачи:")
+    if data == "Сегодня":
+        today.append(task)
+    elif data == "Завтра":
+        tomorrow.append(task)
+    else:
+        other.append(task)
+    print("Задача", task, "на дату", data, "Добавлена")
+
 while run:
     command = input("Введите комманду:")
     if command == "help":
         print(HELP)
     elif command == "show":
-        data1 = input("Введите дату на которую хотите увидеть рассписание:")
-        if data1 == "Сегодня":
-            print(today)
-        elif data1 == "Завтра":
-            print(tomorrow)
-        else:
-            print(other)
+        show()
+
     elif command == "add":
-        task = input("Введите название задачи:")
-        data = input("Введите дату задачи:")
-        if data == "Сегодня":
-            today.append(task)
-        elif data == "Завтра":
-            tomorrow.append(task)
-        else:
-            other.append(task)
-        print("Задача добавлена")
-    elif command == "exit":
-        run = False
+        add()
     else:
         print("Неизвестная комманда")
 
